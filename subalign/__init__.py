44f3656032e69f050aefc65fddacb4c5b6e5fe93
Jaime RGP <jaime.rogue@gmail.com>
2019-02-01 17:00:22 +0100

    use versioneer

--- subalign/__init__.py
+++ subalign/__init__.py
@@ -3,3 +3,6 @@
 
 from __future__ import absolute_import
 from .core import align, align_o3a, align_best, untransformed_rmsd
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
